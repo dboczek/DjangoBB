a4eb403ebcd0f37b1e3d2d41a000d2973c360cc3
Daniel Boczek <daniel.boczek@gmail.com>
2013-04-24 15:16:05 +0200

    version bump

--- djangobb_forum/__init__.py
+++ djangobb_forum/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['__version__', 'get_version']
 
-version_info = (0, 0, 3, 'a', 12)
+version_info = (0, 0, 3, 'a', 13)
 # format: ('major', 'minor', 'micro', 'releaselevel', 'serial')
 # __version__ format compatible with distutils2.version.NormalizedVersion
 
